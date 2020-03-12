5b95dc49bb70cb56db7a35b38e36b0b16b89e3b6
yachen <io260188@gmail.com>
2020-03-12 23:11:28 +0800

    feat: initial 01

--- 01.js
+++ 01.js
@@ -1,21 +1,28 @@
-// 1. 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。
+// 好想電影院規定除非完全沒有客人買票，否則就算只有一位顧客也照常播放電影。
 // 讓使用者輸入客人的人數，並用「!」判斷人數，不為零顯示「照常播放電影」。
 
-const readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+function js01() {
+    const readline = require("readline");
 
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-rl.question("請輸入看電影的人數：", num => {
-    if (num < 0 || isNaN(num)) {
-        console.log("請輸入有效數字");
-    } else if (num == 0) {
-        console.log("不播放電影");
-    } else {
-        console.log("播放電影");
-    }
-    rl.close();
-});
+    rl.question("請輸入人數：", inputValue => {
+        const regularExpression = /[^0-9]/;
+        const numberOfCustomer = regularExpression.test(inputValue);
+        if (numberOfCustomer) {
+            console.log('請輸入正確數字');
+            // return 要加，for recursive。
+            return js01();
+        } else if (inputValue != 0) {
+            console.log("照常播放電影");
+        } else {
+            console.log("不播放電影");
+        }
+    });
+}
+// rl.close();
+js01();
