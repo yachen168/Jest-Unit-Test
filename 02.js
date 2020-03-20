737653e835b103e677c69eb95b55751a9cec4dd9
yachen <io260188@gmail.com>
2020-03-21 00:10:10 +0800

    refactor: modules

--- 02.js
+++ 02.js
@@ -8,23 +8,22 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-const getResult = require('./02.module');
+const { filtNumber, getResult } = require('./02.module');
 
 function js02() {
     rl.question("請輸入第一個整數：", inputNumber1 => {
         rl.question("請輸入第二個整數：", inputNumber2 => {
-            function analyzeData(inputNumber1, inputNumber2) {
-                const Integer = /^-?\d+$/;
-                const isInteger1 = Integer.test(inputNumber1);
-                const isInteger2 = Integer.test(inputNumber2);
-                if (!isInteger1 || !isInteger2) {
-                    console.log('請重新輸入2個有效整數');
-                    return js02();
+            if (!filtNumber(inputNumber1, inputNumber2)) {
+                console.log(`請重新輸入2個有效整數`);
+                return js02();
+            } else {
+                if (getResult(inputNumber1, inputNumber2)) {
+                    console.log(`餘數相同`);
+                } else {
+                    console.log(`餘數不同`);
                 }
-                getResult(inputNumber1, inputNumber2);
-                rl.close();
             }
-            analyzeData(inputNumber1, inputNumber2);
+            rl.close();
         });
     });
 }
--- 02.module.js
+++ 02.module.js
@@ -1,14 +1,16 @@
-function getResult(inputNumber1, inputNumber2) {
-    const remainder1 = Number(inputNumber1) % 3;
-    const remainder2 = Number(inputNumber2) % 3;
-    if (remainder1 === remainder2) {
-        console.log(`餘數相同，皆為 ${remainder1}`);
-        return true;
-    } else {
-        console.log(`餘數不同，第一個餘數為 ${remainder1}，第二個餘數為 ${remainder2}`);
-        return false;
-    }
-
+// 過濾出整數
+function filtNumber(inputNumber1, inputNumber2) {
+    const integer = /^-?\d+$/;
+    const isInteger1 = integer.test(inputNumber1);
+    const isInteger2 = integer.test(inputNumber2);
+    return (isInteger1 && isInteger2);
 }
 
-module.exports = getResult;
+// 計算並判斷餘數
+function getResult(inputNumber1, inputNumber2) {
+    const remainder1 = +inputNumber1 % 3;
+    const remainder2 = +inputNumber2 % 3;
+    return (remainder1 === remainder2);
+}
+
+module.exports = { filtNumber, getResult };
