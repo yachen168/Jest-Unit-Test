4525d32973af205cb03770621563d3419cdf6635
yachen <io260188@gmail.com>
2020-03-21 00:08:52 +0800

    fix: comment

--- 01.module.js
+++ 01.module.js
@@ -1,10 +1,10 @@
-// 正則表達式，過濾出大於等於零的整數
+// 過濾出大於等於零的整數
 function isPositiveInteger(inputNumber) {
     const positiveInteger = /^\d+$/;
     return positiveInteger.test(inputNumber);
 }
 
-// 判斷是否播放電影
+// 是否播放電影
 function isPlayMovie(inputNumber) {
     return +inputNumber;
 }
