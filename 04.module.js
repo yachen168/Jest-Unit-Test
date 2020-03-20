ff2f639861f882a78b10d767603a7d9a31f5b0d4
yachen <io260188@gmail.com>
2020-03-21 03:18:33 +0800

    refactor: -=

--- 04.module.js
+++ 04.module.js
@@ -4,13 +4,14 @@ function isPositiveInteger(inputNumber) {
     return positiveInteger.test(inputNumber);
 }
 
+// return 結果
 function getResult(inputNumber) {
     let equation = "1";
     let sum = 1;
     for (let i = 2; i <= +inputNumber; i++) {
         if (i % 2) {
             equation += `-${i}`;
-            sum += -i;
+            sum -= i;
         } else {
             equation += `+${i}`;
             sum += i;
