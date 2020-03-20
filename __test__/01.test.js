a35befd86bb1816bfb9117a3079714a255d76967
yachen <io260188@gmail.com>
2020-03-20 20:02:39 +0800

    refactor: describe more details

--- __test__/01.test.js
+++ __test__/01.test.js
@@ -1,50 +1,48 @@
-const { filtNumber, getResult } = require('../01.module')
+const { isPositiveInteger, isPlayMovie } = require('../01.module')
     // 因在終端機使用者輸入的值都是 string，所以皆以 string 做測試。
-describe('電影院是否要播放電影', () => {
+describe('JS01', () => {
     describe('過濾使用者輸入的內容', () => {
-        it('輸入符號，要求重新輸入', function() {
-            const case1 = "(´・ω・`)";
-            const result = filtNumber(case1);
-            expect(result).toBeTruthy();
-        })
-        it('輸入英文，要求重新輸入', function() {
-            const case2 = "hello";
-            const result = filtNumber(case2);
-            expect(result).toBeTruthy();
-        })
-        it('輸入負數，要求重新輸入', function() {
-            const case3 = "-10";
-            const result = filtNumber(case3);
-            expect(result).toBeTruthy();
-        })
-        it('輸入小數，要求重新輸入', function() {
-            const case4 = "1.68";
-            const result = filtNumber(case4);
-            expect(result).toBeTruthy();
-        })
-        it('輸入空值，要求重新輸入', function() {
-            const case5 = "";
-            const result = filtNumber(case5);
-            expect(result).toBeTruthy();
-        })
-        it('輸入有效數字，將字串轉為數字', function() {
-            const case6 = "18";
-            const result = filtNumber(case6);
+        it('若輸入任何符號，則要求重新輸入，例如：(´・3・`)', function() {
+            const case1 = "(´・3・`)";
+            const result = isPositiveInteger(case1);
             expect(result).toBeFalsy();
         })
-    })
-    describe('播放電影', () => {
-        it('輸入人數大於 ０ 人，播放電影', function() {
-            const numberOfCustomer = 1;
-            const result = getResult(numberOfCustomer);
+        it('若輸入任何英文，則要求重新輸入，例如：7pupu', function() {
+            const case2 = "7pupu";
+            const result = isPositiveInteger(case2);
+            expect(result).toBeFalsy();
+        })
+        it('若輸入負數，則要求重新輸入，例如：-10', function() {
+            const case3 = "-10";
+            const result = isPositiveInteger(case3);
+            expect(result).toBeFalsy();
+        })
+        it('若輸入小數，則要求重新輸入，例如：1.68', function() {
+            const case4 = "1.68";
+            const result = isPositiveInteger(case4);
+            expect(result).toBeFalsy();
+        })
+        it('若輸入空值，要求重新輸入', function() {
+            const case5 = "";
+            const result = isPositiveInteger(case5);
+            expect(result).toBeFalsy();
+        })
+        it('若輸入有效數字，將字串轉為數字，例如：18', function() {
+            const case6 = "18";
+            const result = isPositiveInteger(case6);
             expect(result).toBeTruthy();
+        })
+    })
+    describe('是否要播放電影', () => {
+        it('若輸入人數大於 0 人，則顯示播放電影', function() {
+            const numberOfCustomer = 1;
+            const result = isPlayMovie(numberOfCustomer);
+            expect(result).toBeTruthy();
+        })
+        it('若輸入人數等於 0 人，則顯示不播放電影', function() {
+            const numberOfCustomer = 0;
+            const result = isPlayMovie(numberOfCustomer);
+            expect(result).toBeFalsy();
         })
     });
-    describe('不播放電影', () => {
-        it('輸入人數 0 人，不播放電影', function() {
-            const numberOfCustomer = 0;
-            const result = getResult(numberOfCustomer);
-            expect(result).toBeFalsy();
-        })
-    })
 });
