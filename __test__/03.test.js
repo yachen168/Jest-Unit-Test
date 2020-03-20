cdf3a6b0556bc41f3223f103230c2cc4ac4397ff
yachen <io260188@gmail.com>
2020-03-21 02:32:20 +0800

    refactor: isPositiveInteger

--- __test__/03.test.js
+++ __test__/03.test.js
@@ -1,36 +1,36 @@
-const { isInteger, isOriginPrice } = require('../03.module');
+const { isPositiveInteger, isOriginPrice } = require('../03.module');
 // 測試採 3A 原則。
 // 因在終端機輸入的值都是 string，所以皆以 string 做測試。
 describe('JS03', () => {
     describe('使用者是否輸入正整數', () => {
         it('若輸入含任何符號，要求重新輸入，例如：(´・3・`)', () => {
             const case1 = "(´・ω・`)";
-            const result = isInteger(case1);
+            const result = isPositiveInteger(case1);
             expect(result).toBeFalsy();
         })
         it('若輸入含任何英文，要求重新輸入，例如：7pupu', () => {
             const case2 = "7pupu";
-            const result = isInteger(case2);
+            const result = isPositiveInteger(case2);
             expect(result).toBeFalsy();
         })
         it('若輸入負數，要求重新輸入，例如：-10', () => {
             const case3 = "-10";
-            const result = isInteger(case3);
+            const result = isPositiveInteger(case3);
             expect(result).toBeFalsy();
         })
         it('若輸入小數，要求重新輸入，例如：1.68', () => {
             const case4 = "1.68";
-            const result = isInteger(case4);
+            const result = isPositiveInteger(case4);
             expect(result).toBeFalsy();
         })
         it('若輸入空值，要求重新輸入', () => {
             const case5 = "";
-            const result = isInteger(case5);
+            const result = isPositiveInteger(case5);
             expect(result).toBeFalsy();
         })
         it('若輸入有效數字，將字串轉為數字，例如：18', () => {
             const case6 = "18";
-            const result = isInteger(case6);
+            const result = isPositiveInteger(case6);
             expect(result).toBeTruthy();
         })
     });
