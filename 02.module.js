6b25df5c4ef91012541043348c43eafac58a6d0a
yachen <io260188@gmail.com>
2020-03-21 16:36:47 +0800

    refactor: remove some variables

--- 02.module.js
+++ 02.module.js
@@ -1,16 +1,12 @@
 // 是否為整數
 function isIntegers(inputNumber1, inputNumber2) {
     const integer = /^-?\d+$/;
-    const isInteger1 = integer.test(inputNumber1);
-    const isInteger2 = integer.test(inputNumber2);
-    return (isInteger1 && isInteger2);
+    return (integer.test(inputNumber1) && integer.test(inputNumber2));
 }
 
 // 餘數是否相同
 function isEqual(inputNumber1, inputNumber2) {
-    const remainder1 = +inputNumber1 % 3;
-    const remainder2 = +inputNumber2 % 3;
-    return (remainder1 === remainder2);
+    return (+inputNumber1 % 3 === +inputNumber2 % 3);
 }
 
 module.exports = { isIntegers, isEqual };
