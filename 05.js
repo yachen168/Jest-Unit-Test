dba645c8d44a67096a7505cc70e0b130bb2f0145
yachen <io260188@gmail.com>
2020-03-22 01:05:22 +0800

    feat: JS05 initial

--- 05.js
+++ 05.js
@@ -1,5 +1,20 @@
 // 寫一個遞迴函數 function umleven(n) 來求算 
 // 2*4 + 4*6 + 6*8...+(n-2)*n
-// (n最小為 4, 只會出現偶數)
+// (n 最小為 4, 只會出現偶數)
 
 const rl = require('./readline.module');
+const { isEven_AtLeastFour, getRecursive } = require('./05.module')
+
+function js05() {
+    rl.question("請輸入一個大於等於 4 的偶數：", n => {
+        if (!isEven_AtLeastFour(n)) {
+            console.log(`請重新輸入一個大於等於 4 的偶數`);
+            return js05();
+        } else {
+            console.log(getRecursive(n));
+        }
+        rl.close();
+    });
+}
+
+js05();
---
+++ 05.module.js
@@ -0,0 +1,16 @@
+// 是否為大於等於 4 的偶數
+function isEven_AtLeastFour(n) {
+    const Integer_AtLeastFour = /^[468][02468]*$|^[1-9][02468]+$/;
+    return Integer_AtLeastFour.test(n);
+}
+
+// 遞迴
+function getRecursive(n) {
+    n = +n;
+    if (n === 4) {
+        return (n - 2) * n;
+    }
+    return getRecursive(n - 2) + (n - 2) * n;
+}
+
+module.exports = { isEven_AtLeastFour, getRecursive };
