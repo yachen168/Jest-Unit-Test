4596e3a81b6424f5b2e0e9071f16d1c4c4f377ff
yachen <io260188@gmail.com>
2020-03-23 00:48:08 +0800

    refactor: remove RegE

--- 08.module.js
+++ 08.module.js
@@ -7,12 +7,13 @@ function isPositiveInteger(inputNumber) {
 function divi(inputNumber, timesOfDivide) {
     timesOfDivide++;
     let resultOfDivide = (inputNumber / 3).toString();
-    const isNotDivisible = /\./;
     const isFoundNumber = /\.[0-9][0]/;
-    if (isNotDivisible.test(resultOfDivide) && !isFoundNumber.test(resultOfDivide)) {
-        return divi(inputNumber / 3, timesOfDivide);
+    if (inputNumber % 3 !== 0 && !isFoundNumber.test(resultOfDivide)) {
+        return divi(resultOfDivide, timesOfDivide);
     }
     return `需除以 ${timesOfDivide} 次`;
 }
+
+//
 
 module.exports = { isPositiveInteger, divi };
