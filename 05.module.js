c1edfcc3bec579a0bcc2f6229b56e63cb3b57236
yachen <io260188@gmail.com>
2020-03-23 10:11:36 +0800

    refactor: conditional operator

--- 05.module.js
+++ 05.module.js
@@ -6,11 +6,7 @@ function isEvenInt_AtLeastFour(n) {
 
 // 遞迴
 function getRecursive(n) {
-    n = +n;
-    if (n > 4) {
-        return getRecursive(n - 2) + (n - 2) * n;
-    }
-    return 2 * 4;
+    return (n > 4) ? getRecursive(n - 2) + (n - 2) * n : 2 * 4;
 }
 
 module.exports = { isEvenInt_AtLeastFour, getRecursive };
