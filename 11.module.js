a7aac04dc695514c2244bf0695c00bf7fd6eac0b
yachen <io260188@gmail.com>
2020-03-24 15:45:59 +0800

    refactor: easier to read

--- 11.module.js
+++ 11.module.js
@@ -7,15 +7,14 @@ function isPositiveInteger(inputNumber) {
 // 取得價錢
 function getPrice(inputNumber) {
     const eachPeriodPrice = 500;
-    const firstPeriodDicount = 500 * (1 - 0.79);
-    let totalPrice = inputNumber * eachPeriodPrice - firstPeriodDicount;
+    const firstPeriodPrice = 500 * 0.79;
+    let totalPrice = firstPeriodPrice + (inputNumber - 1) * eachPeriodPrice;
     if (inputNumber >= 5) {
         do {
             inputNumber -= 5;
             totalPrice -= 200;
         } while (inputNumber > 5);
     }
-
     return `價錢為 ${totalPrice} 元`;
 }
 
