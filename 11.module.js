56da76d4119f79779bf88391d97da12747d5dfa3
yachen <io260188@gmail.com>
2020-03-24 15:39:08 +0800

    fix: do while

--- 11.module.js
+++ 11.module.js
@@ -1,22 +1,22 @@
 // 是否為正整數
 function isPositiveInteger(inputNumber) {
-    const positiveInteger = /^\d+$/;
+    const positiveInteger = /^[1-9]+$/;
     return positiveInteger.test(inputNumber);
 }
 
 // 取得價錢
 function getPrice(inputNumber) {
-    const eachPrice = 500;
-    const firstPeriodPrice = 500 * 0.79;
-    let totalPrice = firstPeriodPrice + (inputNumber - 1) * eachPrice;
-    let isFivePeriodDiscount = (inputNumber - 5) > 0;
-    if (isFivePeriodDiscount) {
+    const eachPeriodPrice = 500;
+    const firstPeriodDicount = 500 * (1 - 0.79);
+    let totalPrice = inputNumber * eachPeriodPrice - firstPeriodDicount;
+    if (inputNumber >= 5) {
         do {
+            inputNumber -= 5;
             totalPrice -= 200;
-            isFivePeriodDiscount -= 1;
-        } while (isFivePeriodDiscount === 0);
+        } while (inputNumber > 5);
     }
-    return `${inputNumber} 期的價錢為 ${totalPrice} 元`;
+
+    return `價錢為 ${totalPrice} 元`;
 }
 
 module.exports = { isPositiveInteger, getPrice };
