2df3756550d5b636ff36bdc744632c0e753667ce
yachen <io260188@gmail.com>
2020-03-31 03:09:46 +0800

    refactor: gradeOfCandidate

--- 17/17.module.js
+++ 17/17.module.js
@@ -1,5 +1,4 @@
 function getResult() {
-    const BordaCount = [4, 4, 4, 4];
     const votes = [51, 5, 23, 21];
     const table = [
         ['candidate1', 'candidate3', 'candidate2', 'candidate4'],
@@ -7,21 +6,16 @@ function getResult() {
         ['candidate2', 'candidate3', 'candidate4', 'candidate1'],
         ['candidate4', 'candidate3', 'candidate2', 'candidate1']
     ];
-    // 四位候選人在各票數的順位
-    const rankingOfCandidate1 = table.map((value) => value.indexOf('candidate1'));
-    const rankingOfCandidate2 = table.map((value) => value.indexOf('candidate2'));
-    const rankingOfCandidate3 = table.map((value) => value.indexOf('candidate3'));
-    const rankingOfCandidate4 = table.map((value) => value.indexOf('candidate4'));
     // 四位候選人在各票數所得分數
-    const grade1 = BordaCount.map((value, index) => value - rankingOfCandidate1[index]);
-    const grade2 = BordaCount.map((value, index) => value - rankingOfCandidate2[index]);
-    const grade3 = BordaCount.map((value, index) => value - rankingOfCandidate3[index]);
-    const grade4 = BordaCount.map((value, index) => value - rankingOfCandidate4[index]);
+    const gradeOfCandidate1 = table.map((value) => 4 - value.indexOf('candidate1'));
+    const gradeOfCandidate2 = table.map((value) => 4 - value.indexOf('candidate2'));
+    const gradeOfCandidate3 = table.map((value) => 4 - value.indexOf('candidate3'));
+    const gradeOfCandidate4 = table.map((value) => 4 - value.indexOf('candidate4'));
     // 候選人的各種票數*各種票數所得分數
-    const result1 = votes.map((value, index) => value * grade1[index]);
-    const result2 = votes.map((value, index) => value * grade2[index]);
-    const result3 = votes.map((value, index) => value * grade3[index]);
-    const result4 = votes.map((value, index) => value * grade4[index]);
+    const result1 = votes.map((value, index) => value * gradeOfCandidate1[index]);
+    const result2 = votes.map((value, index) => value * gradeOfCandidate2[index]);
+    const result3 = votes.map((value, index) => value * gradeOfCandidate3[index]);
+    const result4 = votes.map((value, index) => value * gradeOfCandidate4[index]);
     // 將結果相加
     const accumulator1 = result1.reduce((accumulator, currentValue) => accumulator + currentValue);
     const accumulator2 = result2.reduce((accumulator, currentValue) => accumulator + currentValue);
