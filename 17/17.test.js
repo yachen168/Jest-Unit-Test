d2882c191e0d9f30bbad271fc111b6f7302d2f73
yachen <io260188@gmail.com>
2020-03-30 21:20:35 +0800

    fix: details

--- 17/17.test.js
+++ 17/17.test.js
@@ -2,9 +2,9 @@ const getResult = require('./17.module')
     // 測試採 3A 原則。
 describe('JS17', () => {
     describe('是否正確計算出結果', () => {
-        it('一號選手：253，二號選手：251，三號選手305，三號選手191', () => {
+        it('一號選手：253，二號選手：251，三號選手：305，四號選手：191', () => {
             const result = getResult();
-            expect(result).toMatch(`一號選手：253，二號選手：251，三號選手305，三號選手191`);
+            expect(result).toMatch(`一號選手：253，二號選手：251，三號選手：305，四號選手：191`);
         })
     });
 });
