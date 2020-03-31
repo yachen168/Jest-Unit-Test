8a89e0c673e00cd8a75020afe02660fd86e768a8
yachen <io260188@gmail.com>
2020-03-31 22:34:36 +0800

    refactor: solve magic numbers

--- 16/16.module.js
+++ 16/16.module.js
@@ -3,18 +3,28 @@ function isAlphabet(inputAlphabets) {
     const alphabet = /^[A-z]{5}$/;
     return alphabet.test(inputAlphabets);
 }
+
+// 是否有 XYZxyz
+function isXYZ(inputAlphabets) {
+    const xyz = /[xyz]/i;
+    return xyz.test(inputAlphabets);
+}
+
 // 加密
 function toCaesar(inputAlphabets) {
     let resultOfCaesar = '';
+    const transform = 3;
     for (let index in inputAlphabets) {
         const codeList = inputAlphabets[index].charCodeAt();
-        let caesarCode = String.fromCharCode(codeList + 3);
-        if (codeList >= 88 && codeList <= 90 || codeList >= 120) {
-            caesarCode = String.fromCharCode(codeList - 26 + 3);
+        let caesarCode = String.fromCharCode(codeList + transform);
+        if (isXYZ(inputAlphabets[index])) {
+            caesarCode = String.fromCharCode(codeList + transform - 26);
         }
         resultOfCaesar += caesarCode;
     }
     return resultOfCaesar;
 }
+
+
 
 module.exports = { isAlphabet, toCaesar };
