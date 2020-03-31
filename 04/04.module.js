8ecab2dbe968ad84f0a213af3ddb1f86e523aaef
yachen <io260188@gmail.com>
2020-03-31 22:47:48 +0800

    test: eval()

--- 04/04.module.js
+++ 04/04.module.js
@@ -3,21 +3,17 @@ function isPositiveInteger(inputNumber) {
     const positiveInteger = /^[1-9]\d*$/;
     return positiveInteger.test(inputNumber);
 }
-
 // return 結果
 function getResult(inputNumber) {
     let equation = "1";
-    let sum = 1;
     for (let i = 2; i <= +inputNumber; i++) {
         if (i % 2) {
             equation += `-${i}`;
-            sum -= i;
         } else {
             equation += `+${i}`;
-            sum += i;
         }
     }
-    return `總合：${equation} = ${sum}`;
+    return `總合：${equation} = ${eval(equation)}`;
 }
 
 module.exports = { isPositiveInteger, getResult };
