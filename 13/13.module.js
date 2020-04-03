bcc893db0835b2a02be925d3453c2f7dcadb7a09
yachen <io260188@gmail.com>
2020-04-03 18:02:52 +0800

    refactor: rename

--- 13/13.module.js
+++ 13/13.module.js
@@ -1,7 +1,7 @@
 function getRestTime() {
-    const costOfTime_Turtle = 1000 / 0.28;
-    const costOfTime_Rabbit = 1000 / (20 - 1 / 5);
-    const restTime = Math.floor(costOfTime_Turtle - costOfTime_Rabbit);
+    const costTime_Turtle = 1000 / 0.28;
+    const costTime_Rabbit = 1000 / (20 - 1 / 5);
+    const restTime = Math.floor(costTime_Turtle - costTime_Rabbit);
     return `兔子還可休息 ${restTime} 秒(取整數)`;
 }
 
