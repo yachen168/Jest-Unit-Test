d0c519f5c95d4947f43d4f9cc906d476fd9ccd6b
yachen <io260188@gmail.com>
2020-04-20 19:32:07 +0800

    refactor: Array().fill().map()

--- 12/12.module.js
+++ 12/12.module.js
@@ -1,37 +1,35 @@
 // 印出一陣列 10~20
 function printArray_tenToTwenty() {
-    let array_tenToTwenty = []
-    for (let i = 10; i < 21; i++) {
-        array_tenToTwenty.push(i);
-    }
-    return array_tenToTwenty;
+    return Array(11).fill().map((value, index) => index + 10);
 }
 
 // 找出陣列裡的質數
 function findPrimeNumber() {
-    const primeNumber = printArray_tenToTwenty().filter(value => isPrimeNumber(value));
-    return primeNumber;
+    return printArray_tenToTwenty().filter(value => isPrimeNumber(value));
 }
 
 // 判斷一個數是否為質數
 function isPrimeNumber(value) {
-    for (let i = 2; i < +value; i++) {
-        return !!(+value % i);
+    let primeNumber = true;
+    for (let i = 2; i < value; i++) {
+        if (!(value % i)) {
+            primeNumber = false;
+        }
     }
+    return primeNumber;
 }
 
 // 找出 100 以內倍數
 function getMultiple() {
     let multiple = [];
-    findPrimeNumber().forEach(element => {
+    findPrimeNumber().forEach(value => {
         for (let i = 1; i < 100; i++) {
-            if (i % element === 0) {
+            if (!(i % value)) {
                 multiple.push(i);
             }
         }
     })
     return multiple;
 }
-
 
 module.exports = { findPrimeNumber, getMultiple };
