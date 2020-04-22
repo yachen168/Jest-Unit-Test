1867b768ba2e9b5dddfc02cbad94e1cc0b36dd29
yachen <io260188@gmail.com>
2020-04-22 21:53:33 +0800

    refactor: separate view from model

--- 17/17.js
+++ 17/17.js
@@ -1,5 +1,5 @@
 const rl = require('../readline.module');
-const getResult = require('./17.module');
+const calculate = require('./17.module');
 
-console.log(getResult());
+console.log(`一號選手：${calculate('candidate1')}，二號選手：${calculate('candidate2')}，三號選手：${calculate('candidate3')}，四號選手：${calculate('candidate4')}`);
 rl.close();
--- 17/17.test.js
+++ 17/17.test.js
@@ -1,10 +1,26 @@
-const getResult = require('./17.module')
+const calculate = require('./17.module')
     // 測試採 3A 原則。
 describe('JS17', () => {
     describe('是否正確計算出結果', () => {
-        it('一號選手：253，二號選手：251，三號選手：305，四號選手：191', () => {
-            const result = getResult();
-            expect(result).toMatch(`一號選手：253，二號選手：251，三號選手：305，四號選手：191`);
+        it('一號選手：253', () => {
+            const candidate = 'candidate1';
+            const result = calculate(candidate);
+            expect(result).toBe(253);
+        })
+        it('二號選手：251', () => {
+            const candidate = 'candidate2';
+            const result = calculate(candidate);
+            expect(result).toBe(251);
+        })
+        it('二號選手：251', () => {
+            const candidate = 'candidate3';
+            const result = calculate(candidate);
+            expect(result).toBe(305);
+        })
+        it('二號選手：251', () => {
+            const candidate = 'candidate4';
+            const result = calculate(candidate);
+            expect(result).toBe(191);
         })
     });
 });
