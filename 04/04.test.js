d51987c7b326cf60432ae4e939db29a226eabf34
yachen <io260188@gmail.com>
2020-04-24 19:40:19 +0800

    refactor: rename

--- 04/04.test.js
+++ 04/04.test.js
@@ -4,38 +4,38 @@ const { isPositiveInteger, getResult } = require('./04.module');
 describe('JS04', () => {
     describe('使用者是否輸入大於 0 的整數', () => {
         it('若輸入含任何符號，要求重新輸入，例如：(´・ω・`)', () => {
-            const case1 = "(´・ω・`)";
-            const result = isPositiveInteger(case1);
+            const inputNumber = "(´・ω・`)";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入含任何英文，要求重新輸入，例如：7pupu', () => {
-            const case2 = "7pupu";
-            const result = isPositiveInteger(case2);
+            const inputNumber = "7pupu";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入負數，要求重新輸入，例如：-10', () => {
-            const case3 = "-10";
-            const result = isPositiveInteger(case3);
+            const inputNumber = "-10";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入小數，要求重新輸入，例如：1.68', () => {
-            const case4 = "1.68";
-            const result = isPositiveInteger(case4);
+            const inputNumber = "1.68";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入 0，要求重新輸入', () => {
-            const case3 = "0";
-            const result = isPositiveInteger(case3);
+            const inputNumber = "0";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入空值，要求重新輸入', () => {
-            const case5 = "";
-            const result = isPositiveInteger(case5);
+            const inputNumber = "";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeFalsy();
         })
         it('若輸入有效數字，將字串轉為數字，例如：18', () => {
-            const case6 = "18";
-            const result = isPositiveInteger(case6);
+            const inputNumber = "18";
+            const result = isPositiveInteger(inputNumber);
             expect(result).toBeTruthy();
         })
     });
