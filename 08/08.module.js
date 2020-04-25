6d322eaf369630f6b4a020a823a264fb09082e59
yachen <io260188@gmail.com>
2020-04-25 15:19:05 +0800

    refactor: rename

--- 08/08.module.js
+++ 08/08.module.js
@@ -7,9 +7,9 @@ function isPositiveInteger(inputNumber) {
 function getResultOfDivided(inputNumber, timesOfDivide) {
     let resultAfterDivided = (+inputNumber / 3).toString();
     const dividedWithRemainder = /\./;
-    const ZeroInSecondDecimalPlace = /\.[0-9][0]/;
+    const zeroInSecondDecimalPlace = /\.[0-9][0]/;
     // 若無法被整除且小數點後第二位不為 0 、則繼續除
-    if (dividedWithRemainder.test(resultAfterDivided) && !ZeroInSecondDecimalPlace.test(resultAfterDivided)) {
+    if (dividedWithRemainder.test(resultAfterDivided) && !zeroInSecondDecimalPlace.test(resultAfterDivided)) {
         return getResultOfDivided(inputNumber / 3, ++timesOfDivide);
     }
     return { timesOfDivide, resultAfterDivided };
