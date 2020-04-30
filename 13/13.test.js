dfd1b3ecc29b992744e4922e8cc6ebb064008118
yachen <io260188@gmail.com>
2020-05-01 00:07:06 +0800

    refactor: 3A

--- 13/13.test.js
+++ 13/13.test.js
@@ -3,7 +3,10 @@ const getRestTime = require('./13.module')
 describe('JS13', () => {
     describe('是否正確顯示結果', () => {
         it('顯示結果：兔子還可休息 3520 秒(取整數)', () => {
-            const result = getRestTime();
+            const distance = 1000;
+            const speedPerSecond_Turtle = 0.28;
+            const speedPerSecond_Rabbit = 20 - 1 / 5;
+            const result = getRestTime(speedPerSecond_Turtle, speedPerSecond_Rabbit, distance);
             expect(result).toBe(3520);
         })
     });
