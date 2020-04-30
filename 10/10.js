8242f237985b31d8a8b8c33a49edcc45c0495dbb
yachen <io260188@gmail.com>
2020-05-01 00:08:02 +0800

    refactor: JS10 remove function

--- 10/10.js
+++ 10/10.js
@@ -4,8 +4,11 @@
 const rl = require('../readline.module');
 const { getResult } = require('./10.module');
 
-const message = getResult([3, 50, 0, 13, 2, 4, 11])
-    .map(({ index, value }) => `質數：${value} (索引值為 ${index})`)
-    .join(' , ')
-console.log(message);
+function js10() {
+    const message = getResult([3, 50, 0, 13, 2, 4, 11])
+        .map(({ index, value }) => `質數：${value} (索引值為 ${index})`)
+        .join(' , ')
+    console.log(message);
+}
+js10();
 rl.close();
