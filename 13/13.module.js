36785ed88f8833bd01f3f4f8970c2e80716dfb81
yachen <io260188@gmail.com>
2020-10-19 11:28:01 +0800

    style: code formatting

--- 13/13.module.js
+++ 13/13.module.js
@@ -1,7 +1,7 @@
 function getRestTime(speedPerSecond_Turtle, speedPerSecond_Rabbit, distance) {
-    const costTime_Turtle = distance / speedPerSecond_Turtle;
-    const costTime_Rabbit = distance / speedPerSecond_Rabbit;
-    return Math.floor(costTime_Turtle - costTime_Rabbit);
+  const costTime_Turtle = distance / speedPerSecond_Turtle;
+  const costTime_Rabbit = distance / speedPerSecond_Rabbit;
+  return Math.floor(costTime_Turtle - costTime_Rabbit);
 }
 
 module.exports = getRestTime;
